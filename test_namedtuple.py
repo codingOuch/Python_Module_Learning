108029c43a575ae946a69da084477628e0df5929
haowen zhu <dearzhw@gmail.com>
2019-06-05 18:07:49 +0800

    Temp commit

---
+++ test_namedtuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+import collections
+
+City = collections.namedtuple('name', 'city country population')
+
+tokyo = City('Tokyo', 'Japan', 1000000)
+shanghai = City('Shanghai', 'China', 20000000)
